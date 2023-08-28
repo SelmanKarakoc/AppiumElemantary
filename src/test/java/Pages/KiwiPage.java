8599401369de2d3c9691f440d1e4e2a8ffcc4978
Selman K <selman_karakoc@yahoo.com>
2023-08-28 13:30:09 +0300

    fourth lesson

--- src/test/java/Pages/KiwiPage.java
+++ src/test/java/Pages/KiwiPage.java
@@ -15,7 +15,7 @@ public class KiwiPage {
     public WebElement asaGuest;
 
     @FindBy(id = "com.skypicker.main:id/primary_button")
-    public WebElement continueButton;
+    public WebElement passButton;
 
     @FindBy(id = "com.skypicker.main:id/search_type_label_text_view")
     public WebElement oneWayIdButton;
@@ -35,6 +35,31 @@ public class KiwiPage {
     @FindBy(xpath = "//*[@text='To:']")
     public WebElement toButton;
 
+    @FindBy(id = "com.skypicker.main:id/departure_label_text_view")
+    private WebElement departureButton;
 
+    public void departureButtonClick(){
+        departureButton.click();
+    }
 
+    @FindBy(id = "com.skypicker.main:id/saveButton")
+    public WebElement setDateButton;
+
+    @FindBy(id = "com.skypicker.main:id/search_button")
+    public WebElement searchButton;
+
+    @FindBy(xpath = "//*[@text='Sorting']")
+    public WebElement sortingButton;
+
+    @FindBy(xpath = "//*[@text='Cheapest']")
+    public WebElement cheapestButton;
+
+    @FindBy(xpath = "//*[@text='Stops']")
+    public WebElement stopsButton;
+
+    @FindBy(xpath = "//*[@text='Nonstop']")
+    public WebElement nonStopButton;
+
+    @FindBy(id = "com.skypicker.main:id/price")
+    public WebElement price;
 }
--- src/test/java/Tests/KiwiTest.java
+++ src/test/java/Tests/KiwiTest.java
@@ -2,8 +2,10 @@ package Tests;
 
 import Pages.KiwiPage;
 import Utilize.Driver;
+import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.touch.offset.PointOption;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -22,7 +24,7 @@ public class KiwiTest {
         page.asaGuest.click();
     // ardinda gelecek olan 3 adimada yesil butona basilarak devam edilir
         for (int i = 0; i < 4; i++) {
-            page.continueButton.click();
+            page.passButton.click();
             Thread.sleep(500);
         }
 
@@ -39,11 +41,32 @@ public class KiwiTest {
         page.chooseButton.click();
     // varis ulkesi secenegine tiklanir ve gidilecek ulke girilir
         page.toButton.click();
-        driver.getKeyboard().pressKey("Freiburg");
+        Thread.sleep(500);
+        driver.getKeyboard().pressKey("Basel");
+        driver.findElementByXPath("//*[@text='Basel, Switzerland']").click();
+        page.chooseButton.click();
     // gidis tarihi mayis ayinin 21 i olarak secilir ve set date e tiklanir
+        Thread.sleep(500);
+        page.departureButtonClick();
+        Thread.sleep(500);
+        TouchAction action=new TouchAction<>(driver);
+        action.press(PointOption.point(538,1323)).release().perform();
+        page.setDateButton.click();
+
     // search butonuna tiklanir
+        page.searchButton.click();
+        Thread.sleep(2000);
     // en  ucuz ve aktarmasiz filtrelemeleri yapilir
+        page.sortingButton.click();
+        page.cheapestButton.click();
+        Thread.sleep(3000);
+        page.stopsButton.click();
+        page.nonStopButton.click();
+        Thread.sleep(5000);
     // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
+
+        String price=page.price.getText();
+        driver.sendSMS("12345678",price);
 
 
     }
